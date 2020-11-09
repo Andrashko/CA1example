1ba500a8bdeb430ca73503e46cdfab7ff0b263ac
Andrashko <yurii.andrashko@uzhnu.edu.ua>
2020-11-09 12:30:27 +0200

    task 1 better solve

--- List/task1.py
+++ List/task1.py
@@ -3,17 +3,14 @@ n = int(input("Введіть кількість елементів:"))
 numbers = list(map(float, input(f"Введіть список з {n} дійсних чисел через кому:").split(",")))
 
 firstNegative = 0
+s = 0
 for element in numbers:
     if element < 0:
         firstNegative = element
         break
+    s += element
 
 if firstNegative == 0:
-    print("Відємних елементів в масиві нема")
+    print("Відємний елемент відсутній")
 else:
-    s = 0
-    for element in numbers:
-        if element == firstNegative:
-            break
-        s += element
     print(f"Сума елементів до першого відємного рівна {s}")
